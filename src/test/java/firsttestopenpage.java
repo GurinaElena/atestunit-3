62a9d2943afd62941e1a8d0fe34526da60ba1f11
Gurina Elena <gurina_elena@inbox.ru>
2020-05-21 23:06:20 +0300

    Создание проекта, добавление основных файлов

---
+++ src/test/java/firsttestopenpage.java
@@ -0,0 +1,39 @@
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class firsttestopenpage {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Desktop\\chromedriver.exe");
+    }
+
+         @Test
+        void shouldTest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Гурина Елена");
+        form.$("[data-test-id=phone] input").setValue("+79159638319");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+    }
+}
+
+
+
+
+
+
+
